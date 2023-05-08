7edfbaec3e7d7390de4d7b0e810c6bbb3e17a78b
Kevin Tsang <51415694+kevinchtsang@users.noreply.github.com>
2023-05-08 17:34:37 +0100

    Update example_notebook_r.R

--- example_notebook_r.R
+++ example_notebook_r.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(caret)
 library(skimr)
+library(PRROC)
 
 # normalise function
 
@@ -20,6 +21,9 @@ ar = read_csv('./data/assignment_relationships.csv')
 al = read_csv('./data/action_logs.csv')
 ass_details = read_csv('./data/assignment_details.csv')
 
+# set.seed(123)
+# selected_students = sample(unique(ass_details$student_id), 100)
+
 # Associate the action logs for each in unit assignment with their unit test assignment
 df = ar %>% 
   left_join(al, by = c('in_unit_assignment_log_id' = 'assignment_log_id')) %>% 
@@ -28,7 +32,50 @@ df = ar %>%
 # Create student level data
 
 df_student = al %>%
-  left_join(ass_details, by = "assignment_log_id") 
+  left_join(ass_details, by = "assignment_log_id") %>% 
+  relocate(student_id)
+
+set.seed(123)
+selected_students = sample(unique(df_student$student_id), 1000)
+
+
+df_student_selected = df_student %>%
+  filter(student_id %in% selected_students)
+
+df_student_selected_actions = df_student_selected %>%
+  group_by(student_id, assignment_log_id, action) %>%
+  tally() %>%
+  ungroup()
+
+df_student_selected_unique_actions = df_student_selected_actions %>%
+  group_by(student_id) %>%
+  n_distinct(action)
+
+df_student_selected_features = df_student_selected %>%
+  ungroup() %>%
+  # mutate(
+  #   assignment_duration = ifelse(
+  #     is.na(assignment_end_time),
+  #     NA,
+  #     assignment_end_time - assignment_start_time)
+  # )
+  group_by(student_id) %>%
+  summarise(
+    total_wrong_response = sum(action == "wrong_response"), # total wrong
+    total_assignments = n_distinct(assignment_log_id), # total assignments
+    total_problem = n_distinct(problem_id), # total problem
+    total_action = sum(!is.na(action)) # total action
+    # mean_assignment_duration = mean(assignment_duration)
+    #
+  ) %>%
+  ungroup()
+
+df_student_selected_features = df_student_selected_features %>%
+  mutate(
+    prop_wrong_response = total_wrong_response / total_problem, # ratio
+    prop_action = total_action / total_assignments # ratio
+  )
+
 
 # Get the total number of times each action was taken within the in unit assignments corresponding to each unit test assignment
 df = df %>%
@@ -76,7 +123,9 @@ df_norm = df %>%
 # tuts = tuts.merge(df, how='left', left_on='assignment_log_id', right_index=True)
 
 tuts = tuts %>%
-  left_join(df_norm, by = c('assignment_log_id' = 'unit_test_assignment_log_id'))
+  left_join(df_norm, by = c('assignment_log_id' = 'unit_test_assignment_log_id')) %>%
+  left_join(ass_details[,c('assignment_log_id','student_id')], by = c('assignment_log_id')) %>%
+  left_join(df_student_selected_features, by = c('student_id'))
 
 # Merge action count features with the evaluation unit test scores
 # euts = euts.merge(df, how='left', left_on='assignment_log_id', right_index=True)
@@ -135,6 +184,49 @@ class(tuts$score)
 summary(glm_model2)
 
 
+# train-test-split
+train_students = sample(selected_students, 0.7*length(selected_students))
+test_students = selected_students[!selected_students %in% train_students]
+
+tuts_train = tuts %>%
+  filter(student_id %in% train_students)
+
+tuts_test = tuts %>%
+  filter(student_id %in% test_students)
+
+#
+input_cols3 = colnames(tuts %>% 
+                         select(-c('assignment_log_id',
+                                   'problem_id',
+                                   'score',
+                                   'student_id')))
+input_cols3 = 'action_count'
+
+glm_model3 = glm(formula = paste0(target_col,'~',paste0(input_cols3, collapse = "+")), data = tuts_train)
+
+
+# train models using cross validation
+train.control <- trainControl(method          = "cv", 
+                              number          = 3,
+                              classProbs      = TRUE,
+                              summaryFunction = twoClassSummary)
+
+model_rf <- train(
+  as.factor(make.names(score)) ~ .,
+  data      = tuts_train, 
+  method    = "rf",
+  trControl = train.control,
+  metric    = "ROC")
+
+# rf_model3 = rf(formula = paste0(target_col,'~',paste0(input_cols3, collapse = "+")), data = tuts_train)
+
+
+tuts_test_score = predict(model_rf, 
+                     newdata = tuts_test)
+test_rf_roc = roc.curve(tuts_test_score[tuts_test$score == 1], 
+                        tuts_test_score[tuts_test$score == 0], 
+                        curve = TRUE)
+plot(test_rf_roc)
 
 
 
