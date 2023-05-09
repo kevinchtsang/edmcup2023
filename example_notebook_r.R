7ac076fa124b70d3e55bdbf9d46a37a655c59867
Kevin Tsang <51415694+kevinchtsang@users.noreply.github.com>
2023-05-09 17:10:24 +0100

    final notebook

--- example_notebook_r.R
+++ example_notebook_r.R
@@ -1,5 +1,6 @@
 # Import packages
 
+# library(tidytable)
 library(tidyverse)
 library(caret)
 library(skimr)
@@ -27,31 +28,72 @@ prob_details = read_csv('./data/problem_details.csv')
 # create new variables for prob_details - 1 for prob_skill_code,
 #1 for length of problem skill description
 
-#1
+#1 - create grades
 prob_details <- prob_details %>%
-  separate_wider_delim(problem_skill_code, ".", names = c("problem_grade", "problem_cat", "problem_sub_cat", "problem_#"))
+  arrange(problem_id,problem_multipart_position) %>%
+  fill(problem_skill_code)
+prob_details <- prob_details %>%
+  tidytable::separate_wider_delim(problem_skill_code, ".", names = c("problem_grade", "problem_cat", "problem_sub_cat", "problem_num"))
 
 prob_details$problem_grade <- as.factor(prob_details$problem_grade)
+prob_details$problem_grade_num <- recode(prob_details$problem_grade,
+                                         "K" = "0",
+                                         "HSA" = "9",
+                                         "HSF" = "9",
+                                         "HSG" = "9",
+                                         "HSN" = "9",
+                                         "HSS" = "9")
+prob_details$problem_grade_num = as.numeric(as.character(prob_details$problem_grade_num))
 
 levels(prob_details$problem_grade)
 
-#2
+# HS: introduction, number and quantity, algebra
 
-prob_details$longest_word <- sapply(strsplit(prob_details$problem_skill_description, " "), function(x) x[which.max(nchar(x))])
 
-# str_replace_all(prob_details$longest_word, "[/()]", " ")
+
+#2 - longest word
+
+prob_details$longest_word <- sapply(base::strsplit(prob_details$problem_skill_description, " |/|-"), function(x) x[which.max(nchar(x))])
+prob_details$longest_word <- gsub(",", "", prob_details$longest_word)
+
 
 
 prob_details <- prob_details %>%
   group_by(problem_id) %>%
   mutate(
     length_longest = nchar(longest_word)
-  )
+  ) %>%
+  ungroup()
+
+# ungraded questions
+prob_details$ungraded <- prob_details$problem_type == "Ungraded Open Response"
+
+# View(prob_details[is.na(prob_details$problem_grade),])
 
 ##checking variables
 
-skim(prob_details$length_longest) # range 3-33, hmm...
+# skim(prob_details$length_longest) # range 3-15, we good
+max(prob_details$length_longest)
+min(prob_details$length_longest)
 
+prob_details_selected = prob_details %>%
+  select(problem_id, problem_type, problem_multipart_position,
+         problem_contains_equation,
+        problem_grade_num, length_longest,ungraded)
+
+# make problem_type as dummies
+prob_details_selected$problem_type_i = 1
+prob_details_selected = prob_details_selected %>% 
+  pivot_wider(names_from = problem_type,
+              values_from = problem_type_i,
+              values_fill = 0)
+
+# from the prob_details
+# - problem_multipart_position
+# - problem_type
+# - problem_contains_equations
+# - problem_grade
+# - length_longest
 
 
 # set.seed(123)
@@ -80,25 +122,25 @@ df_student_selected_actions = df_student_selected %>%
   tally() %>%
   ungroup()
 
-df_student_selected_unique_actions = df_student_selected_actions %>%
-  group_by(student_id) %>%
-  n_distinct(action)
+# df_student_selected_unique_actions = df_student_selected_actions %>%
+#   group_by(student_id) %>%
+#   n_distinct(action)
 
 df_student_selected_features = df_student_selected %>%
   ungroup() %>%
-  # mutate(
-  #   assignment_duration = ifelse(
-  #     is.na(assignment_end_time),
-  #     NA,
-  #     assignment_end_time - assignment_start_time)
-  # )
+  mutate(
+    assignment_duration = ifelse(
+      is.na(assignment_end_time),
+      NA,
+      as.numeric(assignment_end_time) - as.numeric(assignment_start_time))
+  ) %>%
   group_by(student_id) %>%
   summarise(
     total_wrong_response = sum(action == "wrong_response"), # total wrong
     total_assignments = n_distinct(assignment_log_id), # total assignments
     total_problem = n_distinct(problem_id), # total problem
-    total_action = sum(!is.na(action)) # total action
-    # mean_assignment_duration = mean(assignment_duration)
+    total_action = sum(!is.na(action)), # total action
+    mean_assignment_duration = mean(assignment_duration, na.rm=T)
     #
   ) %>%
   ungroup()
@@ -110,7 +152,36 @@ df_student_selected_features = df_student_selected_features %>%
   )
 
 
+# df_student_all
+
+df_student_features = df_student %>%
+  ungroup() %>%
+  mutate(
+    assignment_duration = ifelse(
+      is.na(assignment_end_time),
+      NA,
+      as.numeric(assignment_end_time) - as.numeric(assignment_start_time))
+  ) %>%
+  group_by(student_id) %>%
+  summarise(
+    total_wrong_response = sum(action == "wrong_response"), # total wrong
+    total_assignments = n_distinct(assignment_log_id), # total assignments
+    total_problem = n_distinct(problem_id), # total problem
+    total_action = sum(!is.na(action)), # total action
+    mean_assignment_duration = mean(assignment_duration, na.rm=T)
+    #
+  ) %>%
+  ungroup()
+
+df_student_features = df_student_features %>%
+  mutate(
+    prop_wrong_response = total_wrong_response / total_problem, # ratio
+    prop_action = total_action / total_assignments # ratio
+  )
+
+
 # Get the total number of times each action was taken within the in unit assignments corresponding to each unit test assignment
+# dummies
 df = df %>%
   group_by(unit_test_assignment_log_id, action) %>%
   tally() %>%
@@ -123,7 +194,7 @@ df = df %>%
 
 
 # skim variables
-skim(df)
+# skim(df)
 
 # df1 = df %>% get_dummies(cols = 'action') %>%
 #   group_by(unit_test_assignment_log_id) %>%
@@ -158,17 +229,29 @@ df_norm = df %>%
 tuts = tuts %>%
   left_join(df_norm, by = c('assignment_log_id' = 'unit_test_assignment_log_id')) %>%
   left_join(ass_details[,c('assignment_log_id','student_id')], by = c('assignment_log_id')) %>%
-  left_join(df_student_selected_features, by = c('student_id')) %>%
-  left_join(prob_details[c('problem_id', 'problem_type')], by = c("problem_id"))
+  left_join(df_student_features, by = c('student_id')) %>%
+  # left_join(prob_details[c('problem_id', 
+  #                          'problem_type', 
+  #                          'problem_multipart_position',
+  #                          'problem_contains_equation',
+  #                          'problem_grade_num',
+  #                          'length_longest',
+  #                          'ungraded'
+  #                          )], 
+  #           by = c("problem_id"))
+  left_join(prob_details_selected, by = c("problem_id"))
 
-tuts$problem_type = tuts$problem_type %>%
-  replace_na("unknown")
+# tuts$problem_type = tuts$problem_type %>%
+#   replace_na("unknown")
   
 
 # Merge action count features with the evaluation unit test scores
 # euts = euts.merge(df, how='left', left_on='assignment_log_id', right_index=True)
 euts = euts %>%
-  left_join(df_norm, by = c('assignment_log_id' = 'unit_test_assignment_log_id'))
+  left_join(df_norm, by = c('assignment_log_id' = 'unit_test_assignment_log_id')) %>%
+  left_join(ass_details[,c('assignment_log_id','student_id')], by = c('assignment_log_id')) %>%
+  left_join(df_student_features, by = c('student_id')) %>%
+  left_join(prob_details_selected, by = c("problem_id"))
 
 # Collect the input and target columns for the regression
 input_cols = c('action_count')
@@ -229,11 +312,30 @@ euts_submit = euts %>% select(id, score)
 train_students = sample(selected_students, 0.7*length(selected_students))
 test_students = selected_students[!selected_students %in% train_students]
 
-tuts_train = tuts %>%
-  filter(student_id %in% train_students)
+# tuts$problem_type = as.factor(tuts$problem_type)
 
-tuts_test = tuts %>%
-  filter(student_id %in% test_students)
+tuts_ml_df = tuts %>%
+  mutate(across(c(
+    total_wrong_response,
+    total_action,
+    total_assignments,
+    total_action,
+    total_problem,
+    prop_action,
+    problem_multipart_position,
+    problem_grade_num,
+    length_longest,
+    mean_assignment_duration), normalise))
+
+tuts_train = tuts_ml_df %>%
+  filter(student_id %in% train_students) %>%
+  select(-student_id)
+
+tuts_test = tuts_ml_df %>%
+  filter(student_id %in% test_students) %>%
+  select(-student_id)
+
+
 
 #
 input_cols3 = colnames(tuts %>% 
@@ -242,7 +344,9 @@ input_cols3 = colnames(tuts %>%
                                    'score',
                                    'student_id')))
 
-glm_model3 = glm(formula = paste0(target_col,'~',paste0(input_cols3, collapse = "+")), data = tuts_train)
+# glm_model3 = glm(formula = paste0(target_col,'~',paste0(input_cols3, collapse = "+")), data = tuts_train)
+# glm_model3 = glm(formula = paste0(target_col,'~.'), data = tuts_train %>% select(c(score,all_of(input_cols3))))
+glm_model3 = glm(formula = paste0(target_col,'~.'), data = tuts_ml_everything %>% select(c(score,all_of(input_cols3))))
 
 
 # train models using cross validation
@@ -251,19 +355,58 @@ train.control <- trainControl(method          = "cv",
                               classProbs      = TRUE,
                               summaryFunction = twoClassSummary)
 
+# fill NA with 0.5
+tuts_train_filled = tuts_train %>%
+  mutate(across(!assignment_log_id & !problem_id, as.numeric))
+tuts_train_filled[is.na(tuts_train_filled)] = 0.5
+
+tuts_test_filled = tuts_test %>%
+  mutate(across(!assignment_log_id & !problem_id, as.numeric))
+tuts_test_filled[is.na(tuts_test_filled)] = 0.5
+
+# make ML df for everything to train for evaluation
+# tuts
+tuts_ml_everything = tuts_ml_df %>%
+  select(-student_id) %>%
+  mutate(across(!assignment_log_id & !problem_id, as.numeric))
+tuts_ml_everything[is.na(tuts_ml_everything)] = 0.5
+
+# euts
+euts_ml_df = euts %>%
+  mutate(across(c(
+    total_wrong_response,
+    total_action,
+    total_assignments,
+    total_action,
+    total_problem,
+    prop_action,
+    problem_multipart_position,
+    problem_grade_num,
+    length_longest,
+    mean_assignment_duration), normalise))
+euts_ml_everything = euts_ml_df %>%
+  select(-student_id) %>%
+  mutate(across(!assignment_log_id & !problem_id, as.numeric))
+euts_ml_everything[is.na(euts_ml_everything)] = 0.5
+
+
 model_rf <- train(
   as.factor(make.names(score)) ~ .,
-  data      = tuts_train %>% select(c(score,all_of(input_cols3))), 
+  data      = tuts_ml_everything %>% select(c(score,all_of(input_cols3))), 
   method    = "rf",
+  ntree     = 5,
   trControl = train.control,
   metric    = "ROC")
 
 # rf_model3 = rf(formula = paste0(target_col,'~',paste0(input_cols3, collapse = "+")), data = tuts_train)
 
 
-tuts_test_score = predict(model_rf, 
-                     newdata = tuts_test,
+tuts_test_score = predict(model_rf,
+                     newdata = tuts_test_filled,
                      "prob")[,2]
+# tuts_test_score = predict(glm_model3,
+#                           newdata = tuts_test_filled)
+tuts_test_score[is.na(tuts_test_score)] = 0.5
 test_rf_roc = roc.curve(tuts_test_score[tuts_test$score == 1], 
                         tuts_test_score[tuts_test$score == 0], 
                         curve = TRUE)
@@ -271,11 +414,12 @@ plot(test_rf_roc)
 test_rf_roc$auc
 
 
+# eval scores
+euts$score = predict(model_rf,
+                          newdata = euts_ml_everything,
+                          "prob")[,2]
+
+euts_submit = euts %>% select(id, score)
+
 write_csv(euts_submit,
           file = './ushered_example_submission.csv')
-
-<<<<<<< HEAD
-# Python code
-# euts[['id', 'score']].to_csv('./working/example_submission.csv', index=False)
-=======
->>>>>>> 30046d1bb30c42811c4863c56301e5ba9bb1c366
